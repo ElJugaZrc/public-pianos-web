e47e84fa9fd1ecd206e600fe1053c35f79cdf85e
ElJugaZrc <eljugazrc@gmail.com>
2025-07-17 14:27:13 +0200

    Añadido 2 de los 4 botones de la sección de información. Queda el de información pura y el de cerrar el menú.

--- scripts/botones.js
+++ scripts/botones.js
@@ -1,6 +1,6 @@
-document.getElementById("info_buttom").addEventListener("click", function() {
+document.getElementById("info_button").addEventListener("click", function() {
     const panel = document.getElementById("panel_informacion")
-    const info_buttom = document.getElementById("info_buttom")
+    const info_buttom = document.getElementById("info_button")
     panel.style.visibility = "visible";
     panel.style.pointerEvents = "all";
     info_buttom.style.visibility = "hidden";
