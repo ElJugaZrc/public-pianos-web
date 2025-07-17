41f92cbc95b6a382397bb15d35f252396b1c9345
ElJugaZrc <eljugazrc@gmail.com>
2025-07-17 20:09:44 +0200

    Cambiado todo al español

--- scripts/botones.js
+++ scripts/botones.js
@@ -1,6 +1,6 @@
-document.getElementById("info_button").addEventListener("click", function() {
+document.getElementById("boton_info").addEventListener("click", function() {
     const panel = document.getElementById("panel_informacion")
-    const info_buttom = document.getElementById("info_button")
+    const info_buttom = document.getElementById("boton_info")
     panel.style.visibility = "visible";
     panel.style.pointerEvents = "all";
     info_buttom.style.visibility = "hidden";
@@ -9,7 +9,7 @@ document.getElementById("info_button").addEventListener("click", function() {
 
 document.getElementById("boton_cerrar_panel").addEventListener("click", function() {
     const panel = document.getElementById("panel_informacion")
-    const info_buttom = document.getElementById("info_button")
+    const info_buttom = document.getElementById("boton_info")
     panel.style.visibility = "hidden";
     panel.style.pointerEvents = "none";
     info_buttom.style.visibility = "visible";
