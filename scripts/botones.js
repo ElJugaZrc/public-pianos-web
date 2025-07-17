ea25160d7bc850cd47978c1f0d044f73bccd9f09
ElJugaZrc <eljugazrc@gmail.com>
2025-07-17 19:35:31 +0200

    Terminado los 4 botones y añadido el FaQ. Queda por añadir el sistema de markers, saber como poner una caja al clickar los markers, la cajetilla con la leyenda y el server de discord.

--- scripts/botones.js
+++ scripts/botones.js
@@ -6,3 +6,12 @@ document.getElementById("info_button").addEventListener("click", function() {
     info_buttom.style.visibility = "hidden";
     info_buttom.style.pointerEvents = "none";
 })
+
+document.getElementById("boton_cerrar_panel").addEventListener("click", function() {
+    const panel = document.getElementById("panel_informacion")
+    const info_buttom = document.getElementById("info_button")
+    panel.style.visibility = "hidden";
+    panel.style.pointerEvents = "none";
+    info_buttom.style.visibility = "visible";
+    info_buttom.style.pointerEvents = "all";
+})
