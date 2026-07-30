3d9b2333a0c120c0890338ced3f4e2d7374f4fb6
melspowr <177943459+melspowr@users.noreply.github.com>
2026-07-30 01:07:30 +0000

    La leche con el spanglish

--- js/ui.js
+++ js/ui.js
@@ -3,22 +3,22 @@ var boton_cerrar_leyenda = document.getElementById("cerrar_leyenda")
 var boton_abrir_leyenda = document.getElementById("abrir_leyenda")
 
 var panel = document.getElementById("panel_informacion")
-var info_buttom = document.getElementById("boton_info")
+var boton_info = document.getElementById("boton_info")
 
 document.getElementById("boton_info").addEventListener("click", function() {
     /* panel and info_button defined in lines 5 and 6 */
     panel.style.visibility                   = "visible";
     panel.style.pointerEvents                = "all";
-    info_buttom.style.visibility             = "hidden";
-    info_buttom.style.pointerEvents          = "none";
+    boton_info.style.visibility              = "hidden";
+    boton_info.style.pointerEvents           = "none";
 })
 
 document.getElementById("boton_cerrar_panel").addEventListener("click", function() {
     /* panel and info_button defined in lines 5 and 6 */
     panel.style.visibility                   = "hidden";
     panel.style.pointerEvents                = "none";
-    info_buttom.style.visibility             = "visible";
-    info_buttom.style.pointerEvents          = "all";
+    boton_info.style.visibility              = "visible";
+    boton_info.style.pointerEvents           = "all";
 })
 
 boton_cerrar_leyenda.addEventListener("click", function() {
